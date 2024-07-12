3c939900cc6c6efc18e20cd76fa0976cd1a4d22a
thaiacarolainy <carolainenunes22@gmail.com>
2024-07-12 14:08:32 -0300

    correcao do arquivo output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
 output "instance_ami" {
-    value = "aws_instace.web.ami"
+    value = "aws_instace.my_ec2.ami"
 }
 
 output "instance_arn" {
-    value = "aws_instace.web.arn"
+    value = "aws_instace.my_ec2.arn"
 }
 
 output "instance_public_ip" {
     description = "O endereco Ip publico da instancia"
-    value       = aws_instance.web.public_ip
+    value       = aws_instance.my_ec2.public_ip
 }
