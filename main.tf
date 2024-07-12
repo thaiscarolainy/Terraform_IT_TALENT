05a298bbfcd0162fd7fdac93a103674a3b9fd164
thaiacarolainy <carolainenunes22@gmail.com>
2024-07-12 14:16:53 -0300

    correcoes

--- main.tf
+++ main.tf
@@ -1,10 +1,26 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"]
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-06c68f701d8090592"
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
   tags = {
     Name = "ItTalent"
@@ -12,7 +28,7 @@ resource "aws_instance" "my_ec2" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-	bucket = "thaiscarolainy"
+	bucket = "thais_carolainy-it"
 	tags = {
 		Name = "ittalent"
 		Environment = "Test"
