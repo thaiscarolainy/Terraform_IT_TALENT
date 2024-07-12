db7eceee4bae908750c6bb211e01ab526f26fa31
thaiacarolainy <carolainenunes22@gmail.com>
2024-07-12 14:04:51 -0300

    fix: ajustes no arquivo main.tf

--- main.tf
+++ main.tf
@@ -2,25 +2,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"]
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "ItTalent"
@@ -31,5 +15,6 @@ resource "aws_s3_bucket" "my_bucket" {
 	bucket = "thaiscarolainy"
 	tags = {
 		Name = "ittalent"
+		Environment = "Test"
 	}
 }
